e10f41331d8d80f27da5be1e77d1f42ac6788245
OmaraJr <omara1226@gmail.com>
2016-04-06 22:45:48 -0500

    Added ContactUs class to model

--- CHS/Models/ContactUs.cs
+++ CHS/Models/ContactUs.cs
@@ -7,6 +7,9 @@ namespace CHS.Models
 {
     public class ContactUs
     {
-
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public string CellphoneNumber { get; set; }
+        public string HoursOfOperation { get; set; }
     }
 }
